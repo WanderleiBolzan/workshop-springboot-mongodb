24e51f5d6c2ae410941792e192f7e514c1a53428
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-04-11 15:18:56 -0300

    Obtendo um usuário por id

--- src/main/java/com/wanderlei/workshopmongo/service/UserService.java
+++ src/main/java/com/wanderlei/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.wanderlei.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wanderlei.workshopmongo.domain.User;
 import com.wanderlei.workshopmongo.repository.UserRepository;
+import com.wanderlei.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,9 @@ public class UserService {
 	public List<User> findAll() {				
 		return repository.findAll();		
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
