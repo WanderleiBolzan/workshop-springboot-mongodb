073401df1820ee717a8c3c6a0204c885f2919027
wanderlei jose bolzan <wbolzanjp@gmail.com>
2024-04-11 16:44:47 -0300

    Inserção de usuário com POST

--- src/main/java/com/wanderlei/workshopmongo/service/UserService.java
+++ src/main/java/com/wanderlei/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wanderlei.workshopmongo.domain.User;
+import com.wanderlei.workshopmongo.dto.UserDTO;
 import com.wanderlei.workshopmongo.repository.UserRepository;
 import com.wanderlei.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
